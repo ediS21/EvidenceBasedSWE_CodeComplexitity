2fca0aded0ace051da42e9f432282f15d7b64929
Eduard <e.sabo@student.rug.nl>
2024-01-31 21:37:53 +0100

    Pasted cleaned data into original csv, modify diagram for histogram and qqplot

--- diagram.py
+++ diagram.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from scipy.stats import norm
 
 #CHANGE CSV FILE TO DESIRED ONE
 data = pd.read_csv("vlc.csv") 
@@ -23,7 +26,7 @@ plt.title('VLC Studio Average number of lines of code per file')
 plt.xlabel('lines of code')
 plt.ylabel('tag')
 plt.grid(True)
-plt.show()
+#plt.show()
 
 #plot graph
 plt.figure(figsize=(12, 10))
@@ -32,7 +35,7 @@ plt.title('VLC Studio Average cyclomatic complexity per file')
 plt.xlabel('cyclomatic complexity')
 plt.ylabel('tag')
 plt.grid(True)
-plt.show()
+#plt.show()
 
 #plot graph
 plt.figure(figsize=(12, 10))
@@ -41,7 +44,7 @@ plt.title('VLC Studio Average number of functions per file')
 plt.xlabel('number of functions')
 plt.ylabel('tag')
 plt.grid(True)
-plt.show()
+#plt.show()
 
 #plot graph
 plt.figure(figsize=(12, 10))
@@ -50,7 +53,7 @@ plt.title('VLC Studio Sum of number of lines of code per file')
 plt.xlabel('number of lines of code')
 plt.ylabel('tag')
 plt.grid(True)
-plt.show()
+#plt.show()
 
 #plot graph
 plt.figure(figsize=(12, 10))
@@ -59,7 +62,7 @@ plt.title('VLC Studio Sum of cyclomatic complexity per file')
 plt.xlabel('cyclomatic complexity')
 plt.ylabel('tag')
 plt.grid(True)
-plt.show()
+#plt.show()
 
 #plot graph
 plt.figure(figsize=(12, 10))
@@ -68,6 +71,24 @@ plt.title('VLC Studio Sum of number of functions per file')
 plt.xlabel('number of functions')
 plt.ylabel('tag')
 plt.grid(True)
+#plt.show()
+
+# Plot histogram
+plt.subplot(2, 2, 3)
+sns.histplot(sNCSS, bins=30, kde=True, color='skyblue')
+plt.title('Histogram of Average number of lines of code per file')
+plt.xlabel('Lines of Code')
+plt.ylabel('Frequency')
+plt.grid(True)
+#plt.show()
+
+# QQ plot
+plt.subplot(2, 2, 4)
+sm.qqplot(sNCSS, line='s', fit=True)
+plt.title('QQ Plot of Average number of lines of code per file')
+plt.grid(True)
+
+plt.tight_layout()
 plt.show()
 
 #scatter graph
